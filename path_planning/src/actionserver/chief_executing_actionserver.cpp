9853ded1f9dc589a467825095f9ce56939c81a51
valo <valentin.spannagl@student.uni-augsburg.de>
2020-04-07 16:24:55 +0200

    path database #1

---
+++ path_planning/src/actionserver/chief_executing_actionserver.cpp
@@ -0,0 +1,40 @@
+#include "nav_msgs/Path.h"
+#include "filename.h"
+
+
+class path_database
+{
+    public:
+
+        path_database(int #robots)
+        {
+            _#robots = #robots;
+            for (int i = 0; i < #robots; i++)
+            {
+                std::vector<nav_msgs::Path> robot;
+                _path_database.push_back(robot);
+            }
+            
+        }
+
+        CRUD
+        create
+        read
+        update
+        delete
+
+        bool create_path_entry(nav_msgs::Path path, int robot_id)
+        {
+
+            _path_database[robot_id].push_back(path);
+            return true;
+            
+        }
+    
+    private:
+
+        int _#robots;
+        std::vector<std::vector<nav_msgs::Path> > _path_database;
+
+};
+
