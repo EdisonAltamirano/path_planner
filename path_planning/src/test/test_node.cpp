83bdf911e69a2dbba6317fb22c18a5ac5e807c5a
valo <valentin.spannagl@student.uni-augsburg.de>
2020-04-08 08:23:46 +0200

    test node for test reasons

--- path_planning/src/test/test_node.cpp
+++ path_planning/src/test/test_node.cpp
@@ -1,10 +1,27 @@
 #include "ros/ros.h"
+#include "time.h"
+
 
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "test_node");
     
     ros::NodeHandle test_handle;
+
+    ROS_INFO("Walltime: %lf, Rostime: %lf", ros::WallTime::now().toSec(), ros::Time::now().toSec());
+
+    ROS_INFO("Walltime: %lf, Rostime: %lf", ros::WallTime::now().toSec(), ros::Time::now().toSec());
+
+    ros::Rate sleepy(1);
+    while (ros::ok())
+    {
+            ROS_INFO("Walltime: %lf, Rostime: %lf", ros::WallTime::now().toSec(), ros::Time::now().toSec());
+            sleepy.sleep();
+
+    }
+    
+
+    
 
     ros::spin();
     
